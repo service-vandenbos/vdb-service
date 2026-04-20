749e8c95d7deb85a05d01c35f661ec9446b4111a
service-vandenbos <service@vandenbos-cm.nl>
2026-04-20 16:02:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 window.APP_CONFIG = {
   supabaseUrl: 'https://tbtoaeraevuygajyaggj.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRidG9hZXJhZXZ1eWdhanlhZ2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1MDUyNzksImV4cCI6MjA5MDA4MTI3OX0.D8gKf3hIwQnP8GULWgN2pINrttdR_oUc_WSK50SQl9c',
-  tableName: 'service_tickets'
+  tables: {
+    customers: 'customers',
+    machines: 'machines',
+    service_events: 'service_events'
+  }
 };
